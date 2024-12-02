a355f82135a0054deff25a9b1bdfba72adb20d8e
omar-merhebi <116896797+omar-merhebi@users.noreply.github.com>
2024-12-02 18:04:46 -0500

    started on tokenize script

---
+++ tokenize.py
@@ -0,0 +1,16 @@
+import nltk
+import pandas as pd
+
+from collections import Counter
+
+
+def main():
+    wine_reviews = pd.read_csv('data/processed/wine-reviews-cleaned.csv')
+
+
+def tokenize_description(description):
+    pass
+
+
+if __name__ == '__main__':
+    main()
