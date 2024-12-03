20e7c2eb212ce177369e262bf2f9acfe756edebc
omar-merhebi <116896797+omar-merhebi@users.noreply.github.com>
2024-12-03 10:05:03 -0500

    remove title col after preprocessing

--- preprocess.R
+++ preprocess.R
@@ -3,8 +3,8 @@ library(tidyverse)
 wine_reviews <- read.csv('data/raw/wine-reviews.csv')
 
 wine_reviews <- wine_reviews %>%
-  select(-c('X', 'designation', 'region_1', 'region_2', 'taster_name', 'winery')) %>%
   mutate(year = str_extract(title, "20[0-2][1-9]")) %>%
+  select(-c('X', 'designation', 'region_1', 'region_2', 'taster_name', 'winery', 'title')) %>%
   filter(taster_twitter_handle != "") %>%
   rename(taster = taster_twitter_handle) %>%
   mutate(taster = str_extract(taster, "\\w+")) %>%
