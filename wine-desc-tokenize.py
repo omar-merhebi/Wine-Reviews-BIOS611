a1ff317c994432eb0c63cd245932b8d63086cb99
omar-merhebi <116896797+omar-merhebi@users.noreply.github.com>
2024-12-03 11:10:27 -0500

    worked on this more

--- wine-desc-tokenize.py
+++ wine-desc-tokenize.py
@@ -1,5 +1,6 @@
 import nltk
 import pandas as pd
+import re
 import string
 
 from collections import Counter
@@ -7,31 +8,66 @@ from collections import Counter
 nltk.download('punkt')
 nltk.download('punkt_tab')
 nltk.download('stopwords')
+nltk.download('wordnet')
 
 STOP_WORDS = set(nltk.corpus.stopwords.words('english'))
 
 
 def main():
     wine_reviews = pd.read_csv('data/processed/wine-reviews-clean.csv')
+    wine_word_counts = get_word_counts_per_wine(wine_reviews)
+    total_word_counts = get_total_word_count(wine_word_counts)
 
-    example_description = "This is ripe and fruity, a wine that is smooth while still structured. Firm tannins are filled out with juicy red berry fruits and freshened with acidity. It's  already drinkable, although it will certainly be better from 2016."
+    # print(total_word_counts)
 
-    tokenize_description(example_description)
-    
 
-def tokenize_description(description):
+def get_total_word_count(wine_word_counts):
+    # We get the total word counts for each word across all wine descriptions.
+    total_word_count = Counter()
+
+    for word_counts in wine_word_counts:
+        print(word_counts)
+        total_word_count.update(word_counts)
+
+    return total_word_count
+
+
+def get_word_counts_per_wine(wine_reviews):
+    wine_word_count = {}
+
+    for index, row in wine_reviews.iterrows():
+        description = row['description']
+
+        word_counts = tokenize_and_count(description)
+
+        wine_word_count[index] = word_counts
+
+    return wine_word_count
+
+
+def tokenize_and_count(description):
     # make lowercase and get rid of punctuation
     text = description.translate(
         str.maketrans("", "", string.punctuation)).lower()
 
+    # remove numbers (this usually indicates a year for the wine which)
+    # we already extracted from the title.
+    text = re.sub(r'\d+', '', text)
+
     # tokenize
     tokens = nltk.word_tokenize(text)
 
-    print(tokens)
+    # lemmatize words so that associated words get grouped together
+    # For example, group "cherry" and "cherries" or "beauty" and "beautiful"
+    lemmatizer = nltk.stem.WordNetLemmatizer()
+    tokens = [lemmatizer.lemmatize(token) for token in tokens]
+
     # remove stop words
     tokens = [word for word in tokens if word not in STOP_WORDS]
-    
-    print(tokens)
+
+    word_counts = Counter(tokens)
+
+    return word_counts
 
 
 if __name__ == '__main__':
