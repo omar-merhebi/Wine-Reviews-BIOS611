57df9f5099555c1118482439d7748922481c9bf7
omar-merhebi <116896797+omar-merhebi@users.noreply.github.com>
2024-12-04 15:36:32 -0500

    remove blank countries

--- preprocess.R
+++ preprocess.R
@@ -5,6 +5,7 @@ wine_reviews <- read.csv('data/raw/wine-reviews.csv')
 wine_reviews <- wine_reviews %>%
   mutate(year = str_extract(title, "20[0-2][1-9]")) %>%
   select(-c('X', 'designation', 'region_1', 'region_2', 'taster_name', 'winery', 'title')) %>%
+  filter(!is.na(country) & country != '') %>%
   filter(taster_twitter_handle != "") %>%
   rename(taster = taster_twitter_handle) %>%
   mutate(taster = str_extract(taster, "\\w+")) %>%
