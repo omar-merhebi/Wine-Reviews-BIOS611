92dede6edf0eb711bdeecaa8cd1ccc7cc1c6ee24
omar-merhebi <116896797+omar-merhebi@users.noreply.github.com>
2024-12-04 14:38:37 -0500

    this is actually going to make a figure showing the top 25 words.

---
+++ top-25-words.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+
+wine_reviews <- read_csv('data/processed/wine-reviews-tokenized.csv')
+
+wine_reviews <- wine_reviews %>%
+  rename(ID = `...1`)
+
+wine_reviews_for_pca <- wine_reviews %>%
+  select(-c(Country, Points, Price, Province, Taster, Variety, Year))
+
+gathered_wine_reviews <- wine_reviews_for_pca %>%
+  gather(key="word", value="count", -ID)
+
+gathered_wine_total_counts <- gathered_wine_reviews %>%
+  select(-ID) %>%
+  group_by(word) %>%
+  summarise(total_count=sum(count))
+
+top_25_words_plot <- gathered_wine_total_counts %>%
+  top_n(25, wt=total_count) %>%
+  ggplot(aes(x=reorder(word, -total_count), y=total_count)) +
+  geom_col() +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle=90, vjust=0.5, hjust=1)) +
+  labs(x="Word", y="Total Count")
+
+top_25_words_plot
