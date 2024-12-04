fc03d35d7f6bdfc4989000e283af44fa744c6827
omar-merhebi <116896797+omar-merhebi@users.noreply.github.com>
2024-12-04 16:35:15 -0500

    train price model

--- train-nn.py
+++ train-nn.py
@@ -1,16 +1,41 @@
 import tensorflow as tf
 import pandas as pd
 
+from sklearn.model_selection import train_test_split
+
 
 def main():
     # read in data
     data = pd.read_csv('data/processed/wine-reviews-nn.csv')
+    train_nn(data)
 
 
 def train_nn(data):
     X = data.drop(columns=['Price', 'Points'], inplace=False)
     y_price = data['Price']
     y_points = data['Points']
+
+    # train test split
+    X_train, X_test, y_price_train, y_price_test = train_test_split(
+        X, y_price, test_size=0.2, random_state=416)
+
+    X_train, X_test, y_points_train, y_points_test = train_test_split(
+        X, y_points, test_size=0.2, random_state=416
+    )
+
+    input_dim = X_train.shape[1]
+
+    price_model = build_model(input_dim)
+    points_model = build_model(input_dim)
+
+    print('Training Price Prediction Model')
+    price_model.fit(X_train, y_price_train, epochs=20, batch_size=100,
+                    validation_split=0.2)
+
+    print('Evaluating Price Prediction Model on Test Data')
+    price_loss, price_mae = price_model.evaluate(X_test, y_price_test)
+    print(f'Price Prediction Loss: {price_loss}')
+    print(f'Price Prediction MAE: {price_mae}')
 
 
 def build_model(input_dim):
