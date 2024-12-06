d6ff9e0eaac28d9ae45ef6408b5da11c077aac98
omar-merhebi <116896797+omar-merhebi@users.noreply.github.com>
2024-12-06 11:44:41 -0500

    remove outliers in price

--- preprocess.R
+++ preprocess.R
@@ -11,4 +11,9 @@ wine_reviews <- wine_reviews %>%
   mutate(taster = str_extract(taster, "\\w+")) %>%
   drop_na()
 
+# Let's drop some outliers in terms of price.
+# Any wine over $500 will be considered an outlier 
+wine_reviews <- wine_reviews %>%
+  filter(price <= 500)
+
 write_csv(wine_reviews, 'data/processed/wine-reviews-clean.csv')
