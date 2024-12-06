3987798903e48cddf496223b94096acf62c0ed8f
omar-merhebi <116896797+omar-merhebi@users.noreply.github.com>
2024-12-06 12:10:04 -0500

    first scatter

--- analyze-nn-resutls.R
+++ analyze-nn-resutls.R
@@ -1,5 +1,11 @@
 library(tidyverse)
 
 # Here we create figures that look at the results of the neural networks
-net_predictions <- read.csv('data/results/wine_predictions.csv')
+net_predictions <- read.csv('data/results/wine_predictions.csv') %>%
+  select(-X)
 
+# Scatter plots of predicted vs actual values
+net_predictions %>%
+  ggplot(aes(x=price, y=price_pred)) +
+  geom_point() + theme_bw() +
+  geom_abline(slope=1)
