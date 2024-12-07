151940de4f890fbee73689bcf6126e35a3e44ba9
omar-merhebi <116896797+omar-merhebi@users.noreply.github.com>
2024-12-07 16:46:45 -0500

    plot MAEs

--- nn-result-scatterplots.R
+++ nn-result-scatterplots.R
@@ -4,20 +4,27 @@ library(Metrics)
 # Here we create figures that look at the results of the neural networks
 net_predictions <- read.csv('data/results/wine_predictions.csv')
 
-price_mae <- mae(net_predictions$price, net_predictions$price_pred)
-points_mae <- mae(net_predictions$points, net_predictions$points_pred)
+price_mae <- round(mae(net_predictions$price, net_predictions$price_pred), 3)
+points_mae <- round(mae(net_predictions$points, net_predictions$points_pred), 3)
 
 # Scatter plots of predicted vs actual values
 price_scatter <- net_predictions %>%
   ggplot(aes(x=price, y=price_pred)) +
   geom_point(color='lightblue') + theme_bw() +
   geom_abline(slope=1, color='red') +
-  theme(panel.grid = element_blank())
+  theme(panel.grid = element_blank()) +
+  annotate("text", x=max(net_predictions$price)-1, 
+           y=max(net_predictions$price_pred),
+           label=paste("MAE: ", price_mae))
 
 points_scatter <- net_predictions %>%
   ggplot(aes(x=points, y=points_pred)) + 
   geom_point(color='lightblue') + theme_bw() +
-  geom_abline(slope=1, color='red')
+  geom_abline(slope=1, color='red') +
+  theme(panel.grid = element_blank()) +
+  annotate("text", x=max(net_predictions$points)-1, 
+           y=max(net_predictions$points_pred),
+           label=paste("MAE: ", points_mae))
 
 ggsave('figures/price_prediction_scatterplot.png', plot=price_scatter)
 ggsave('figures/points_prediction_scatterplot.png', plot=points_scatter)
