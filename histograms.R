078722bd1855b013c41ec15e2ff3ee5d8018d073
omar-merhebi <116896797+omar-merhebi@users.noreply.github.com>
2024-12-07 15:10:23 -0500

    finalize histogram aesthetics

--- histograms.R
+++ histograms.R
@@ -5,12 +5,15 @@ wine_reviews <- read.csv('data/processed/wine-reviews-clean.csv')
 continents <- wine_reviews %>%
   ggplot(aes(x=Continent, fill=Continent)) +
   geom_bar() +theme_bw() +
-  theme(panel.grid = element_blank())
+  theme(panel.grid = element_blank(),
+        legend.position = 'none') +
+  labs(x='Continent', y='Wine Count')
 
 prices_w_outliers <- wine_reviews %>%
   ggplot(aes(x=price)) +
-  geom_histogram() + theme_bw() +
-  theme(panel.grid = element_blank())
+  geom_histogram(fill='lightblue', color='white', binwidth=50) + theme_bw() +
+  theme(panel.grid = element_blank()) +
+  labs(x='Price', y='Wine Count', title = 'With Outliers') 
 
 # Calculate upper outlier threshhold 
 Q1 <- quantile(wine_reviews$price, 0.25)
@@ -21,20 +24,22 @@ threshhold <- Q3 + 1.5 * IQR
 
 prices_w_outliers <- prices_w_outliers + 
   geom_vline(xintercept = threshhold, color='red', linetype='dashed') +
-  annotate("text", x=threshhold+10, color='red', y=40000, 
+  annotate("text", x=threshhold+100, color='red', y=40000, 
            label=paste(threshhold))
 
 # Now do the histogram after outlier removal
 prices <- wine_reviews %>%
   filter(price <= 77) %>%
   ggplot(aes(x=price)) +
-  geom_histogram() + theme_bw() +
-  theme(panel.grid = element_blank())
+  geom_histogram(fill='lightblue', color='white') + theme_bw() +
+  theme(panel.grid = element_blank()) +
+  labs(x='Price', y='Wine Count', title='Without Outliers')
 
 points <- wine_reviews %>%
   ggplot(aes(x=points)) +
-  geom_histogram(binwidth = 1) + theme_bw() +
-  theme(panel.grid = element_blank())
+  geom_histogram(binwidth = 1, fill='lightblue', color='white') + theme_bw() +
+  theme(panel.grid = element_blank()) +
+  labs(x='Points', y='Wine Count')
 
 ggsave('figures/histogram-by-continent.png', continents)
 ggsave('figures/histogram-by-price-full.png', prices_w_outliers)
