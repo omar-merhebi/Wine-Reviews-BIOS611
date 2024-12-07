65b9647ab363f9cc559b6e980c36f5009acb194f
omar-merhebi <116896797+omar-merhebi@users.noreply.github.com>
2024-12-07 16:05:34 -0500

    dont do separate PCA without europe it makes no difference

--- pca.R
+++ pca.R
@@ -15,16 +15,4 @@ pca <- wine_reviews %>%
 
 pca <- autoplot(pca, data=wine_reviews, color='Continent') + theme_bw()
 
-# PCA excluding europe
-
-wine_reviews_no_europe <- wine_reviews %>%
-  filter(Continent != 'Europe')
-
-pca_no_europe <- wine_reviews_no_europe %>%
-  select(-c(ID, Country, Continent)) %>%
-  prcomp(scale. = TRUE)
-
-pca_no_europe <- autoplot(pca_no_europe, data=wine_reviews_no_europe, color='Continent') + theme_bw()
-
 ggsave('figures/pca-by-continent.png', plot=pca)
-ggsave('figures/pca-by-continent-exclude-eur.png', plot=pca_no_europe)
