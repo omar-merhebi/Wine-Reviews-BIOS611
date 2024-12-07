318f794da8965070c0552f3bd49c6a74417766c7
omar-merhebi <116896797+omar-merhebi@users.noreply.github.com>
2024-12-07 13:39:13 -0500

    finish join script

--- join-post-prediction-data.R
+++ join-post-prediction-data.R
@@ -3,5 +3,11 @@ library(tidyverse)
 original_data <- read.csv('data/processed/wine-reviews-clean.csv')
 nn_results <- read.csv('data/results/wine_predictions.csv')
 
+to_join <- original_data %>%
+  mutate(ID = row_number()) %>%
+  select(c(country, variety, year, Continent, ID))
+
 joined_data <- nn_results %>%
-  left_join(original_data, by=)
+  left_join(to_join, by='ID')
+
+write_csv(joined_data, 'data/results/wine_predictions_joined.csv')
